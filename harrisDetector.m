9a3b704be110caae29c15bd9be93fb232927da7e
Marissa <dalonzo.m@husky.neu.edu>
2019-10-17 10:39:59 -0400

    fix harris bugs

--- harrisDetector.m
+++ harrisDetector.m
@@ -1,6 +1,7 @@
 function [Rs] = harrisDetector(image, nonMaxThreshold, nonMaxRange)
-% Calculate gradient masks 
-[Ix,Iy] = imgradientxy(image, 'prewitt'); 
+
+% Calculate gradient masks
+[Ix,Iy] = imgradientxy(image, 'prewitt');
 
 % calculate gradient products
 IxIx = Ix.*Ix;
@@ -8,89 +9,93 @@ IyIy = Iy.*Iy;
 IxIy = Ix.*Iy;
 
 
-% Sum in 3x3 window 
-filter = ones(3,3); 
-IxIx = imfilter(IxIx, filter, 'replicate'); 
-IxIy = imfilter(IxIy, filter, 'replicate'); 
-IyIy = imfilter(IyIy, filter, 'replicate'); 
+% Sum in 3x3 window
+filter = ones(3,3);
+IxIx = imfilter(IxIx, filter, 'replicate');
+IxIy = imfilter(IxIy, filter, 'replicate');
+IyIy = imfilter(IyIy, filter, 'replicate');
 
-% Compute C matrix and R matrix 
-eigenvalues = {}; 
-k = .06; 
-Rs = zeros(size(Ix)); 
+% Compute C matrix and R matrix
+k = .04;
+Rs = zeros(size(Ix));
 for i = 1:size(Ix,1)
     for j = 1:size(Ix,2)
-        C = [IxIx(i,j) IxIy(i,j); IxIy(i,j) IyIy(i,j)]; 
-        eigs = eig(double(C)); 
-        R = eigs(1)*eigs(2) - (k * (eigs(1) + eigs(2))^2); 
-        Rs(i,j) = R; 
-    end 
-end  
+        C = [IxIx(i,j) IxIy(i,j); IxIy(i,j) IyIy(i,j)];
+        eigs = eig(double(C));
+        R = eigs(1)*eigs(2) - (k * (eigs(1) + eigs(2))^2);
+        Rs(i,j) = R;
+    end
+end
 
-%% Nonmax suppression 
-% Calculate magnitude and direction of gradient 
+%% Nonmax suppression
+% Calculate magnitude and direction of gradient
 [Imag, Idir] = imgradient(image, 'prewitt');
-directions = [-180, -135, -90, -45, 0, 45, 90, 135, 180];   
-for i = 1:size(Rs)
-    for j = 1:size(Rs)
-        if Rs(i,j) > nonMaxThreshold 
-            % Find closest direction in magnitude of gradient 
-            direction = min(abs(Rs(i,j) - directions)); 
-            for k = 1:nonMaxRange 
-                switch direction 
-                    case -180
-                        if (Rs(i-k, j) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break 
-                        end 
-                    case -135 
-                        if (Rs(i-k, j-k) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break
-                        end 
-                    case -90 
-                        if (Rs(i, j-k) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break
-                        end
-                    case -45 
-                        if (Rs(i+k, j-k) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break
-                        end 
-                    case 0 
-                        if (Rs(i+k, j) > Rs(i,j)) 
-                            Rs(i,j) = 0; 
-                            break
-                        end 
-                    case 45 
-                        if (Rs(i+k, j+k) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break
-                        end 
-                    case 90 
-                        if (Rs(i+k, j) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break
-                        end 
-                    case 135 
-                        if (Rs(i+k, j+k) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break
-                        end 
-                    case 180 
-                        if (Rs(i-k, j) > Rs(i,j))
-                            Rs(i,j) = 0; 
-                            break 
-                        end 
-                end
-            end 
-      
-        % Not sure if this should be here 
-        else 
-            Rs(i,j) = 0;
-        end 
-    end 
-end 
+directions = [-180, -135, -90, -45, 0, 45, 90, 135, 180];
+for i = 1:size(Rs,1)
+    for j = 1:size(Rs,2)
+        % Find closest direction in magnitude of gradient
+        [diff, index] = min(abs(Rs(i,j) - directions));
+        direction = directions(index);
+        for k = 1:nonMaxRange
+            switch direction
+                case -180
+                    if i-k > 0 && (Rs(i-k,j) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case -135
+                    if i-k > 0 && j-k > 0 && (Rs(i-k, j-k) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case -90
+                    if j-k > 0 && (Rs(i, j-k) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case -45
+                    if i+k <= size(Rs,1) && j-k > 0 && (Rs(i+k, j-k) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case 0
+                    if  i+k <= size(Rs,1) && (Rs(i+k, j) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case 45
+                    if  i+k <= size(Rs,1) &&  j+k <= size(Rs,2) && (Rs(i+k, j+k) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case 90
+                    if  j+k <= size(Rs,2) && (Rs(i, j+k) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case 135
+                    if i+k <= size(Rs,1) &&  j+k <= size(Rs,2) && (Rs(i+k, j+k) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+                case 180
+                    if i-k > 0 && (Rs(i-k, j) > Rs(i,j))
+                        Rs(i,j) = 0;
+                        break
+                    end
+            end
+        end
+    end
+    
+end
 
-end 
+for i = 1:size(Rs,1)
+    for j = 1:size(Rs,2)
+        if Rs(i,j) ~= 0 
+            Rs(i,j) = Imag(i,j); 
+        end
+        if Rs(i,j)  < nonMaxThreshold
+            Rs(i,j) = 0;
+        end
+    end
+end
--- project2.m
+++ project2.m
@@ -1,15 +1,28 @@
 clear 
 clc 
 %% Read in images
-dir = fullfile('/Users/marissadalonzo/Documents/MATLAB/ComputerVision/Lab4/DanaHallWay2');
+dir = fullfile('/Users/marissadalonzo/Documents/MATLAB/ComputerVision/Project2/DanaHallWay2');
 files = imageDatastore(dir);
 image1 = rgb2gray(readimage(files,1)); 
 image2 = rgb2gray(readimage(files,2)); 
 
 %% Harris detector 
-Rs1 = harrisDetector(image1, 10, 3); 
-Rs2 = harrisDetector(image2, 10, 3); 
+Rs1 = harrisDetector(image1, 100, 10); 
+Rs2 = harrisDetector(image2, 100, 10); 
 
+%% NCC 
+filter = ones(3,3); 
+image1squared = Rs1 .* Rs1; 
+image2squared = Rs2 .* Rs2; 
+
+image1squared = imfilter(image1squared, filter, 'replicate'); 
+image2squared = imfilter(image2squared, filter, 'replicate');
+
+image1squared = image1squared.^0.5; 
+image2squared = image2squared.^0.5; 
+
+Rs1 = Rs1 ./image1squared; 
+Rs2 = Rs2 ./image2squared; 
 
 
         
