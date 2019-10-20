9a580a0fd2fd05d3d040dfe6dcff33ea57c56f76
ryanhubel <ryanhub@att.net>
2019-10-20 12:30:34 -0400

    Fixed NCC, increased window, homography estimation, and basic warping

---
+++ test.m
@@ -0,0 +1,12 @@
+imageOrig = imread('DanaHallWay2/DSC_0285.JPG');
+imageGray = rgb2gray(imageOrig);
+Rs = harrisDetector(imageGray, 100, 20);
+image('CData', imageOrig,'XData',[1 512], 'YData', [-1 -340])
+for i = 1:size(Rs,1)
+    for j = 1:size(Rs,2)
+        if(Rs(i,j)>=150)
+            hold on
+            plot(j,-i,'o')
+        end
+    end
+end
