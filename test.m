ccfdb0f7c963f1d7b811722ccb0be074b17cd975
ryanhubel <ryanhub@att.net>
2019-10-21 20:21:21 -0400

    Adjusing corner marker size

--- test.m
+++ test.m
@@ -1,13 +1,13 @@
 imageOrig = imread('DanaHallWay2/DSC_0285.JPG');
 imageGray = rgb2gray(imageOrig);
 figure; 
-Rs = harrisDetector(imageGray, 100, 100);
+Rs = harrisDetector(imageGray, 100);
 image('CData', imageOrig,'XData',[1 512], 'YData', [-1 -340])
 for i = 1:size(Rs,1)
     for j = 1:size(Rs,2)
         if(Rs(i,j)>=150)
             hold on
-            plot(j,-i,'o')
+            plot(j,-i,'.','MarkerSize',40)
         end
     end
 end
